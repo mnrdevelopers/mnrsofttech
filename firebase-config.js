9c639f3fdf9b18ac4de6480677475018c8c72c4c
mnrdevelopers <136944017+mnrdevelopers@users.noreply.github.com>
2025-10-11 19:25:13 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCsgmsgUpMgb5Pw8xA_R3i9ybt6iEpNQ64",
+  authDomain: "mnr-soft-tech-invoice.firebaseapp.com",
+  projectId: "mnr-soft-tech-invoice",
+  storageBucket: "mnr-soft-tech-invoice.firebasestorage.app",
+  messagingSenderId: "846761019349",
+  appId: "1:846761019349:web:98adfefb8ac2b44f115f5c",
+  measurementId: "G-HTGPVDVPCR"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+const auth = firebase.auth();
+const db = firebase.firestore();
